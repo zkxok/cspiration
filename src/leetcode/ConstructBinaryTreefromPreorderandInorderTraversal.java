4ff143f02c23850709ffbb5161a21a2c057fc25d
张可新 <19329831+zkxok@users.noreply.github.com>
2019-11-13 21:13:42 +0800

    Update ConstructBinaryTreefromPreorderandInorderTraversal.java

--- src/leetcode/ConstructBinaryTreefromPreorderandInorderTraversal.java
+++ src/leetcode/ConstructBinaryTreefromPreorderandInorderTraversal.java
@@ -27,22 +27,20 @@ public class ConstructBinaryTreefromPreorderandInorderTraversal {
      * @return
      */
     public TreeNode buildTree(int[] preorder, int[] inorder) {
-        return helper(0, 0, inorder.length - 1, preorder, inorder);
+        return helper(preorder,inorder,0,0,inorder.length-1);
     }
 
-    public TreeNode helper(int preStart, int inStart, int inEnd, int[] preorder, int[] inorder) {
-        if (preStart > preorder.length - 1 || inStart > inEnd) {
+    public TreeNode helper(int[] preorder,int[] inorder,int preStart,int inStart,int inEnd){
+        if(preStart>preorder.length-1||inStart>inEnd){//preStart>preorder.length-1||可加可不加
             return null;
         }
         TreeNode root = new TreeNode(preorder[preStart]);
-        int inIndex = 0;
-        for (int i = inStart; i <= inEnd; i++) {
-            if (inorder[i] == root.val) {
-                inIndex = i;
-            }
+        int inIndex = 1;
+        for(int i=inStart;i<=inEnd;i++){
+            if(inorder[i]==root.val) inIndex  = i;
         }
-        root.left = helper(preStart + 1, inStart, inIndex - 1, preorder, inorder);
-        root.right = helper(preStart + inIndex - inStart + 1, inIndex + 1, inEnd, preorder, inorder);
+        root.left = helper(preorder,inorder,preStart+1,inStart,inIndex-1);
+        root.right = helper(preorder,inorder,preStart+inIndex-inStart+1,inIndex+1,inEnd);
         return root;
     }
 
