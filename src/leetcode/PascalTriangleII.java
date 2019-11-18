5e18a0b3db287050a451797d23ad5ddac968ca34
张可新 <19329831+zkxok@users.noreply.github.com>
2019-11-18 13:17:33 +0800

    Update PascalTriangleII.java

--- src/leetcode/PascalTriangleII.java
+++ src/leetcode/PascalTriangleII.java
@@ -30,13 +30,13 @@ public class PascalTriangleII {
      * @param rowIndex
      * @return
      */
+    //这里的rowIndex是索引,范围从0开始
     public List<Integer> getRow(int rowIndex) {
-        List<Integer> res = new ArrayList<>();
-        if (rowIndex < 0) return res;
-        for (int i = 0; i < rowIndex + 1; i++) {
-            res.add(0, 1);
-            for (int j = 1; j < res.size() - 1; j++) {
-                res.set(j, res.get(j) + res.get(j + 1));
+        List<Integer> res = new ArrayList();
+        for(int i=0;i<rowIndex+1;i++){
+            res.add(0,1);
+            for(int j=1;j<res.size()-1;j++){
+                res.set(j,res.get(j)+res.get(j+1));
             }
         }
         return res;
