701973ffa28630e82dc82182c7da39ce0c2610f5
张可新 <19329831+zkxok@users.noreply.github.com>
2019-11-18 12:27:39 +0800

    Update PopulatingNextRightPointersinEachNodeII.java

--- src/leetcode/PopulatingNextRightPointersinEachNodeII.java
+++ src/leetcode/PopulatingNextRightPointersinEachNodeII.java
@@ -133,4 +133,30 @@ public class PopulatingNextRightPointersinEachNodeII {
         return root;
     }
     
+    public Node connect(Node root) {
+        if (root == null) return null;
+        if (root.left != null) {
+            if (root.right != null) {
+                root.left.next = root.right;
+            } else {
+                root.left.next = nextNode(root.next);
+            }
+        }
+        if (root.right != null) {
+            root.right.next = nextNode(root.next);
+        }
+        connect(root.right);
+        connect(root.left);
+        return root;
+    }
+
+    private Node nextNode(Node node) {
+        while (node != null) {
+            if (node.left != null)  return node.left;
+            if (node.right != null) return node.right;
+            node = node.next;
+        }
+        return null;
+    }
+    
 }
