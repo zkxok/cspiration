1dcece19f5ecdfb45f8a0b5c2479c43f3a0c0c9e
张可新 <19329831+zkxok@users.noreply.github.com>
2019-11-18 11:27:16 +0800

    Update PopulatingNextRightPointersinEachNodeII.java

--- src/leetcode/PopulatingNextRightPointersinEachNodeII.java
+++ src/leetcode/PopulatingNextRightPointersinEachNodeII.java
@@ -105,4 +105,32 @@ public class PopulatingNextRightPointersinEachNodeII {
         }
         return root;
     }
+    
+    
+    
+    public Node connect(Node root) {
+        if (root == null) {
+            return null;
+        }
+        // 借助队列实现层次遍历
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            while (size-- > 0) {
+                Node cur = queue.poll();
+                if (size > 0) {
+                    cur.next = queue.peek();
+                }
+                if (cur.left != null) {
+                    queue.offer(cur.left);
+                }
+                if (cur.right != null) {
+                    queue.offer(cur.right);
+                }
+            }
+        }
+        return root;
+    }
+    
 }
