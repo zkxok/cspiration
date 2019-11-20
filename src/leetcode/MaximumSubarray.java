5f986ecf873af453552e245a2085ce68542253ab
张可新 <19329831+zkxok@users.noreply.github.com>
2019-11-20 23:56:25 +0800

    Update MaximumSubarray.java

--- src/leetcode/MaximumSubarray.java
+++ src/leetcode/MaximumSubarray.java
@@ -44,3 +44,39 @@ public class MaximumSubarray {
         return res;
     }
 }
+
+
+
+class Solution {
+    public int maxSubArray(int[] nums) {
+        if(nums==null||nums.length==0) return 0;
+        int sum = nums[0];
+        int maxSum =nums[0];
+        for(int i=1;i<nums.length;i++){
+            if(sum>0){
+                sum+=nums[i];
+            }else{
+                sum=nums[i];
+            }
+            maxSum = Math.max(maxSum,sum);
+        }
+        return maxSum;
+    }
+
+
+
+    public int maxSubArray(int[] nums) {
+        int res = nums[0];
+        int sum =  nums[0];
+        for(int i= 1;i<nums.length;i++){
+            if(sum>0){
+                sum=sum+nums[i];
+            }else{
+                sum = nums[i];
+            }
+            res = Math.max(res,sum);
+        }
+        return res;
+    }
+}
+
