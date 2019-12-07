c181f597f0f672ce07898153d8f75b6f1a834709
张可新 <19329831+zkxok@users.noreply.github.com>
2019-12-08 02:12:11 +0800

    Update and rename MajorityElement.java to 169MajorityElement.java

--- src/leetcode/169MajorityElement.java
+++ src/leetcode/169MajorityElement.java
@@ -46,7 +46,7 @@ public class MajorityElement {
         return res;
     }
 
-    // Moore voting algorithm
+    // Moore voting algorithm(前提是数组中一定是有元素超过半数的，正好此题是如此)
     // 每次都找出一对不同的元素，从数组中删掉，直到数组为空或只有一种元素。
     // 不难证明，如果存在元素e出现频率超过半数，那么数组中最后剩下的就只有e。
     // [1,2,3,3,3]
