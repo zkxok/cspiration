f0aaa27edc56c07ae9c78edf7ad3fa8fec41aaf8
张可新 <19329831+zkxok@users.noreply.github.com>
2019-12-09 23:06:19 +0800

    Update 201BitwiseANDofNumbersRange.java

--- src/leetcode/201BitwiseANDofNumbersRange.java
+++ src/leetcode/201BitwiseANDofNumbersRange.java
@@ -25,12 +25,13 @@ public class BitwiseANDofNumbersRange {
      */
 
     public int rangeBitwiseAnd(int m, int n) {
-        int offset = 0;
-        while (m != n) {
-            m >>= 1;
-            n >>= 1;
+        int offset=0;
+        //只要出现奇偶数,对应位置一定是0,因为是连续的，所以一定会有奇偶数,唯一可能为1的一位就是首位
+        while(m!=n){
+            m>>=1;
+            n>>=1;
             offset++;
         }
-        return m << offset;
+        return m<<offset;//或者m<<offset
     }
 }
