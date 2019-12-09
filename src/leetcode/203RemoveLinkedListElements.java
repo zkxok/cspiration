8fa8161feb5a90df45919be13ddf21d69ab6db6a
张可新 <19329831+zkxok@users.noreply.github.com>
2019-12-10 01:31:16 +0800

    Update 203RemoveLinkedListElements.java

--- src/leetcode/203RemoveLinkedListElements.java
+++ src/leetcode/203RemoveLinkedListElements.java
@@ -70,5 +70,19 @@ public class RemoveLinkedListElements {
         }
         return dummy.next;
     }
+    
+    public ListNode removeElements5(ListNode head, int val) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode pre = dummy;
+        while(pre.next!=null){
+            if(pre.next.val==val) {
+                pre.next = pre.next.next;
+            }else{
+                pre = pre.next;
+            }
+        }
+        return dummy.next;
+    }
 }
 
