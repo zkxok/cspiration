f251a5befe8c9b3c97099ad0f74c1766a404e29d
张可新 <19329831+zkxok@users.noreply.github.com>
2019-12-14 22:19:12 +0800

    Update 218TheSkylineProblem.java

--- src/leetcode/218TheSkylineProblem.java
+++ src/leetcode/218TheSkylineProblem.java
@@ -84,50 +84,7 @@ public class TheSkylineProblem {
     
     
     ****************
-   public List<List<Integer>> getSkyline3(int[][] buildings) {
-		List<List<Integer>> res = new ArrayList<>();
-		List<int[]> heights = new ArrayList<>();
-		for (int[] b : buildings) {
-			heights.add(new int[] { b[0], -b[2] });
-			heights.add(new int[] { b[1], b[2] });
-		}
-		// Collections.sort(heights, (a, b) -> (a[0] == b[0]) ? a[1] - b[1] : a[0] - b[0]);
-		Collections.sort(heights, new Comparator<int[]>() {
-			@Override
-			public int compare(int[] a, int[] b) {
-				if (a[0] == b[0])
-					return a[1] - b[1];
-				return a[0] - b[0];
-			}
-		});
-		// PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> (b - a));
-		PriorityQueue<Integer> pq = new PriorityQueue<>(new Comparator<Integer>() {
-			@Override
-			public int compare(Integer a, Integer b) {
-				return b - a;
-			}
-		});
-		pq.offer(0);
-		int prev = 0;
-		for (int[] h : heights) {
-			if (h[1] < 0) {
-				pq.offer(-h[1]);
-			} else {
-				pq.remove(h[1]);
-			}
-			int cur = pq.peek();
-			if (prev != cur) {
-				res.add(Arrays.asList(h[0], cur));
-//				res.add(new int[] { h[0], cur });
-				prev = cur;
-			}
-		}
-		return res;
-	}
-	
-	
-	
-   public List<List<Integer>> getSkyline4(int[][] buildings) {
+    public static List<List<Integer>> getSkyline3(int[][] buildings) {
         List<List<Integer>> res = new ArrayList<>();
         List<int[]> heights = new ArrayList<>();
         for (int[] b: buildings) {
@@ -142,7 +99,6 @@ public class TheSkylineProblem {
 				else return a[0] - b[0];
 			}
 		});
-
         TreeMap<Integer, Integer> map = new TreeMap<>(Collections.reverseOrder());
         map.put(0,1);
         int prev = 0;
@@ -160,7 +116,46 @@ public class TheSkylineProblem {
             int cur = map.firstKey();
             if (prev != cur) {
 //                res.add(new int[]{h[0], cur});
-		res.add(Arrays.asList(h[0], cur));
+				res.add(Arrays.asList(h[0], cur));
+                prev = cur;
+            }
+        }
+        return res;
+    }
+	
+	
+   public List<List<Integer>> getSkyline4(int[][] buildings) {
+        List<List<Integer>> res = new ArrayList<>();
+        List<int[]> heights = new ArrayList<>();
+        for (int[] b: buildings) {
+            heights.add(new int[]{b[0], - b[2]});
+            heights.add(new int[]{b[1], b[2]});
+        }
+//        Collections.sort(heights, (a, b) -> (a[0] == b[0]) ? a[1] - b[1] : a[0] - b[0]);
+        Collections.sort(heights, new Comparator<int[]>() {
+			@Override
+			public int compare(int[] a, int[] b) {
+				if(a[0] == b[0]) return a[1] -b[1];
+				else return a[0] - b[0];
+			}
+		});
+        TreeMap<Integer, Integer> map = new TreeMap<>(Collections.reverseOrder());
+        map.put(0,1);
+        int prev = 0;
+        for (int[] h: heights) {
+            if (h[1] < 0) {
+                map.put(-h[1], map.getOrDefault(-h[1], 0) + 1);
+            } else {
+                int cnt = map.get(h[1]);
+                if (cnt == 1) {
+                    map.remove(h[1]);
+                } else {
+                    map.put(h[1], cnt - 1);
+                }
+            }
+            int cur = map.firstKey();
+            if (prev != cur) {
+        		res.add(Arrays.asList(h[0], cur));
                 prev = cur;
             }
         }
