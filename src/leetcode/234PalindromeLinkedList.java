ea576f12ba4db7cf5865775d2f05a0223f3e13aa
张可新 <19329831+zkxok@users.noreply.github.com>
2019-12-16 03:18:26 +0800

    Update 234PalindromeLinkedList.java

--- src/leetcode/234PalindromeLinkedList.java
+++ src/leetcode/234PalindromeLinkedList.java
@@ -51,4 +51,40 @@ public class PalindromeLinkedList {
         }
         return prev;
     }
+    
+    **************************
+    public boolean isPalindrome(ListNode head) {
+		if (head == null) return true;
+		ListNode middle = findMiddle(head);
+		middle = reverse(middle);
+		ListNode p = head;
+		ListNode q = middle;
+		while (p != null && q != null) {
+			if (p.val != q.val) return false;
+			p = p.next;
+			q = q.next;
+		}
+		return true;
+	}
+
+	public ListNode findMiddle(ListNode head) {
+		ListNode slow = head;
+		ListNode fast = head;
+		while (fast != null && fast.next != null) {
+			slow = slow.next;
+			fast = fast.next.next;
+		}
+		return slow;
+	}
+
+	public ListNode reverse(ListNode head) {
+		ListNode prev = null;
+		while (head != null) {
+			ListNode temp = head.next;
+			head.next = prev;
+			prev = head;
+			head = temp;
+		}
+		return prev;
+	}
 }
