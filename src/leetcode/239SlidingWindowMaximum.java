ac594f21cc6596a05268fb6b056b4649a956b84e
张可新 <19329831+zkxok@users.noreply.github.com>
2019-12-18 04:44:37 +0800

    Update 239SlidingWindowMaximum.java

--- src/leetcode/239SlidingWindowMaximum.java
+++ src/leetcode/239SlidingWindowMaximum.java
@@ -56,4 +56,30 @@ public class SlidingWindowMaximum {
         }
         return res;
     }
+
+
+***************上文注释版******************
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        if (nums == null || nums.length == 0) {
+            return new int[0];
+        }
+        Deque<Integer> deque = new LinkedList<>();
+        int[] res = new int[nums.length + 1 - k];
+        for (int i = 0; i < nums.length; i++) {
+            if (!deque.isEmpty() && deque.peekFirst() <= i - k) {//==也行
+                deque.poll();//移除所有滑动窗口范围之外的元素,移除队头,等同于removeFirst
+            }
+            //必须是peekLast,不能是peekFirst和removeFirst,
+            //如果是peekFirst和removeFirst，则输入[1,3,1,2,0,5],3 实际输出[3,3,1,5],正确结果:[3,3,2,5]
+            while (!deque.isEmpty() && nums[deque.peekLast()] < nums[i]) {
+                deque.removeLast();//移除比当前元素小的所有元素,只要有当前元素在,它们就不可能是最大的
+                //从末尾移除,因为队列是从大到小排列的（队头->队尾），要移除所有比当前元素小的，必然从末尾开始判断删除
+            }
+            deque.offerLast(i);
+            if ((i + 1) >= k) {
+                res[i + 1 - k] = nums[deque.peek()];//队头的元素为总是为滑动窗口最大的元素
+            }
+        }
+        return res;
+    }
 }
