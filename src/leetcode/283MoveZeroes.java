90a348002a8f7417e7f50091445290ad250ae798
张可新 <19329831+zkxok@users.noreply.github.com>
2019-12-19 23:40:01 +0800

    Update 283MoveZeroes.java

--- src/leetcode/283MoveZeroes.java
+++ src/leetcode/283MoveZeroes.java
@@ -62,4 +62,20 @@ public class MoveZeroes {
             }
         }
     }
+    
+    *******************************************
+    public void moveZeroes(int[] nums) {
+        if(nums == null || nums.length==0) return;
+        for(int i =0,j=0; i<nums.length; i++){
+            if(nums[i]!=0){
+                swap(nums,i,j++);
+            }
+        }
+    }
+
+    public void swap(int nums[],int i, int j){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
 }
