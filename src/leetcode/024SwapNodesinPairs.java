df9b3cf54175216bc2a81e354b22ef5558bddbda
张可新 <19329831+zkxok@users.noreply.github.com>
2019-12-22 18:45:13 +0800

    Update 024SwapNodesinPairs.java

--- src/leetcode/024SwapNodesinPairs.java
+++ src/leetcode/024SwapNodesinPairs.java
@@ -30,6 +30,7 @@ public class SwapNodesinPairs {
         ListNode l2 = head;
         while (l2 != null && l2.next != null) {
             ListNode nextStart = l2.next.next;
+            //交换操作
             l1.next = l2.next;
             l2.next.next = l2;
             l2.next = nextStart;
