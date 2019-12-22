2a880059b519e57d030f74c05e9c1a48d8240b85
张可新 <19329831+zkxok@users.noreply.github.com>
2019-12-23 01:39:38 +0800

    Update 426ConvertBinarySearchTreeToSortedDoublyLinkedList.java

--- src/leetcode/426ConvertBinarySearchTreeToSortedDoublyLinkedList.java
+++ src/leetcode/426ConvertBinarySearchTreeToSortedDoublyLinkedList.java
@@ -18,15 +18,15 @@ class Node {
     }
 };
 */
-class Solution {
-    //与剑指offer27题，唯一区别就是，剑指offer不需要成环，这里成环了
-  Node first = null;//realHead
-	Node last = null;//tail
+public class Solution {
+	// 与剑指offer27题，唯一区别就是，剑指offer不需要成环，这里成环了
+	Node first = null;// realHead
+	Node last = null;// tail
 
 	public Node treeToDoublyList(Node root) {
 		if (root == null) return root;
 		helper(root);
-        //连接首尾元素,形成一个环
+		// 连接首尾元素,形成一个环
 		last.right = first;
 		first.left = last;
 		return first;
@@ -36,20 +36,21 @@ class Solution {
 		if (root == null) return;
 		helper(root.left);
 		// if (last != null) {
-		// 	last.right = node;
-		// 	node.left = last;
+		// last.right = root;
+		// root.left = last;
 		// } else {
-		// 	first = node;
+		// first = root;
 		// }
-        if (last == null) {
+		if (last == null) {
 			last = root;
-            first = root;
+			first = root;
 		} else {
 			last.right = root;
-            root.left = last;
-            last = root;
+			root.left = last;
+			last = root;
 		}
-		// last = node;
+		// last = root;
 		helper(root.right);
 	}
+
 }
