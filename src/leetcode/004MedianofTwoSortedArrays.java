a513cc63d24343038a33913c3be82f59b8e281e1
张可新 <19329831+zkxok@users.noreply.github.com>
2019-12-24 16:43:52 +0800

    Update 004MedianofTwoSortedArrays.java

--- src/leetcode/004MedianofTwoSortedArrays.java
+++ src/leetcode/004MedianofTwoSortedArrays.java
@@ -105,4 +105,43 @@ public class MedianofTwoSortedArrays {
         }
         return -1;
     }
+    
+    
+    
+    **********上面的方案加注释版*******************
+	public double findMedianSortedArrays3(int[] nums1, int[] nums2) {
+		if (nums1.length > nums2.length) {//保证nums1的长度一定是小于等于nums2的长度的
+			return findMedianSortedArrays(nums2, nums1);
+		}
+		int len = nums1.length + nums2.length;
+		int cut1 = 0;//数组1的切割位mid
+		int cut2 = 0;//数组2的切割位
+		//因为需要进行二分查找
+		int cutL = 0;//数组1的start
+		int cutR = nums1.length;//数组1的end
+		while (cut1 <= nums1.length) {
+			cut1 = (cutR - cutL) / 2 + cutL;//对数组1进行二分查找，每次从二分的中间位置cut1开始切割
+			cut2 = len / 2 - cut1;//根据数组1的切割位置cut1,来计算数组2的切割位置cut2
+			double L1 = (cut1 == 0) ? Integer.MIN_VALUE : nums1[cut1 - 1];
+			double L2 = (cut2 == 0) ? Integer.MIN_VALUE : nums2[cut2 - 1];
+			double R1 = (cut1 == nums1.length) ? Integer.MAX_VALUE : nums1[cut1];
+			double R2 = (cut2 == nums2.length) ? Integer.MAX_VALUE : nums2[cut2];
+			//三种情况
+			if (L1 > R2) {
+				cutR = cut1 - 1;
+			} else if (L2 > R1) {
+				cutL = cut1 + 1;
+			} else {//L1<=R2 L2<=R1,cut1的位置是正确的
+				if (len % 2 == 0) {//长度是偶数,那么中位数是两数平均(L1,L2,R1,R2四个数中，处于中间大小的两个数的平均值)
+					L1 = L1 > L2 ? L1 : L2;
+					R1 = R1 < R2 ? R1 : R2;
+					return (L1 + R1) / 2;
+				} else {//长度是奇数时,那么中位数是cu1位的数R1=nums1[cu1]或cut2位的数R2=nums2[cut2]中较小的那个
+					R1 = (R1 < R2) ? R1 : R2;
+					return R1;
+				}
+			}
+		}
+		return -1;
+	}
 }
