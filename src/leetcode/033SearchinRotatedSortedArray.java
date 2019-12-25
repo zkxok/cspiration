d9481e8dbba7dd37ae95bfa17c0214e10ed801c0
张可新 <19329831+zkxok@users.noreply.github.com>
2019-12-26 03:10:08 +0800

    Update 033SearchinRotatedSortedArray.java

--- src/leetcode/033SearchinRotatedSortedArray.java
+++ src/leetcode/033SearchinRotatedSortedArray.java
@@ -51,4 +51,27 @@ public class SearchinRotatedSortedArray {
         if (nums[end] == target) return end;
         return -1;
     }
+    
+    
+    public int search2(int[] nums, int target) {
+		if (nums == null || nums.length == 0) return -1;
+		int start = 0;
+		int end = nums.length - 1;
+		while (start + 1 < end) {//至少3个数参与,如果1个数或者2个数，不用这么麻烦
+			int mid = (end - start) / 2 + start;
+			if (nums[mid] == target) {
+				return mid;
+			} else if (nums[start] < nums[mid]) {// start->mid 升序
+				if (nums[start] <= target && target <= nums[mid]) end = mid;
+				else start = mid + 1;
+			} else if (nums[mid] < nums[end]) {// mid->end升序
+				if (nums[mid] <= target && target <= nums[end]) start = mid;
+				else end = mid - 1;
+			}
+		}
+		//能够走到这里说明只有1个数或者2个数
+		if (nums[start] == target) return start;
+		if (nums[end] == target) return end;
+		return -1;
+	}
 }
