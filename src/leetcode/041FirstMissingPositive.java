fc2568e009a048cde9988dfa6707562540d83564
张可新 <19329831+zkxok@users.noreply.github.com>
2019-12-25 17:16:48 +0800

    Update 041FirstMissingPositive.java

--- src/leetcode/041FirstMissingPositive.java
+++ src/leetcode/041FirstMissingPositive.java
@@ -29,6 +29,8 @@ public class FirstMissingPositive {
         for (int i = 0; i < nums.length; i++) {
             //不是排序,小于等于0的数不能参与排序
             //如果正常nums[nums[i] - 1]应该等于nums[i],当前nums[i]的值本应该在索引为(nums[i] - 1)的位置,如果不在，那就将它交换到那个位置
+            //这里是while,不是if,还有nums[nums[i] - 1] != nums[i]不能写成nums[i] - 1!=i,如果这样写输入[1,1],nums[i] - 1!=i,这个条件将永远满足
+            //会陷入死循环
             while (nums[i] > 0 && nums[i] <= nums.length && nums[nums[i] - 1] != nums[i]) {
                 int temp = nums[nums[i] - 1];
                 nums[nums[i] - 1] = nums[i];
