668a92b0940ebc5e6ed2efdd1db4bf9158a4243c
张可新 <19329831+zkxok@users.noreply.github.com>
2019-12-27 02:47:42 +0800

    Update 055JumpGame.java

--- src/leetcode/055JumpGame.java
+++ src/leetcode/055JumpGame.java
@@ -22,10 +22,10 @@ public class JumpGame {
      * @return
      */
     public boolean canJump(int[] nums) {
-        int max = 0;
+        int max = 0;//当前能走到的最大(远)位置的索引
         for (int i = 0; i < nums.length; i++) {
-            if (i > max) return false;
-            max = Math.max(nums[i] + i, max);
+            if (i > max) return false;//i>max说明它走不到i这个位置
+            max = Math.max(nums[i] + i, max);//nums[i] + i是以当前这个点为起点能走到哪个位置
         }
         return true;
     }
