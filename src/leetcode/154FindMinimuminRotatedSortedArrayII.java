082fbc3ab6d4ddea0766482d92e19f1ef573e627
张可新 <19329831+zkxok@users.noreply.github.com>
2019-12-26 22:56:31 +0800

    Update 154FindMinimuminRotatedSortedArrayII.java

--- src/leetcode/154FindMinimuminRotatedSortedArrayII.java
+++ src/leetcode/154FindMinimuminRotatedSortedArrayII.java
@@ -41,13 +41,13 @@ public class FindMinimuminRotatedSortedArrayII {
     
     
     public int findMin2(int[] nums) {
-        int left = 0, right = nums.length - 1;
-        while (left < right) {
-            int mid = (left + right) / 2;
-            if (nums[mid] > nums[right]) left = mid + 1;
-            else if (nums[mid] < nums[right]) right = mid;
-            else right = right - 1;
+        int start = 0, end = nums.length - 1;
+        while (start < end) {
+            int mid = (end - start) / 2 + start;
+            if (nums[mid] > nums[end]) start = mid + 1;
+            else if (nums[mid] < nums[end]) end = mid;
+            else end = end - 1;
         }
-        return nums[left];
+        return nums[start];
     }
 }
