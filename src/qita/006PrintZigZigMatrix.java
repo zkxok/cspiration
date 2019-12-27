226c5a87b90cd6adc55081aba1faa39e53ecb559
张可新 <19329831+zkxok@users.noreply.github.com>
2019-12-28 05:28:45 +0800

    Update 006PrintZigZigMatrix.java

--- src/qita/006PrintZigZigMatrix.java
+++ src/qita/006PrintZigZigMatrix.java
@@ -1,36 +1,41 @@
+package ti;
+
+import java.util.*;
+
 //判题OJ:https://www.nowcoder.com/questionTerminal/7df39c7556424eada267d8f793961a1e?toCommentId=5162493
 public class Test {// 之字形打印矩阵
-	public static List<Integer> printMatrix(int[][] matrix) {
-		List<Integer> res = new ArrayList<>();
-		if (matrix == null || matrix.length == 0 || matrix[0] == null || matrix[0].length == 0) return res;
-		int m = matrix.length;
-		int n = matrix[0].length;
-		for (int i = 0; i < m; i++) {
-			for (int j = 0; j < n; j++) {
-				if (i % 2 == 0) {// 奇数行
-					res.add(matrix[i][j]);
-				} else {// 偶数行
-					res.add(matrix[i][n - 1 - j]);
-				}
-			}
-		}
-		return res;
-	}
 
-	public int[] printMatrix2(int[][] matrix, int m, int n) {
-		int[] res = new int[m * n];
-		if (matrix == null || matrix.length == 0 || matrix[0] == null || matrix[0].length == 0) return res;
-		int count = 0;
-		for (int i = 0; i < m; i++) {
-			for (int j = 0; j < n; j++) {
-				if (i % 2 == 0) {// 奇数行
-					res[count++] = matrix[i][j];
-				} else {// 偶数行  
-					res[count++] = matrix[i][n - 1 - j];
-				}
-			}
+    public static List<Integer> printMatrix(int[][] matrix) {
+	List<Integer> res = new ArrayList<>();
+	if (matrix == null || matrix.length == 0 || matrix[0] == null || matrix[0].length == 0) return res;
+	int m = matrix.length;
+	int n = matrix[0].length;
+	for (int i = 0; i < m; i++) {
+	    for (int j = 0; j < n; j++) {
+		if (i % 2 == 0) {// 奇数行
+			res.add(matrix[i][j]);
+		} else {// 偶数行
+			res.add(matrix[i][n - 1 - j]);
 		}
-		return res;
+	    }
 	}
+	return res;
+    }
+
+    public int[] printMatrix2(int[][] matrix, int m, int n) {
+	int[] res = new int[m * n];
+	if (matrix == null || matrix.length == 0 || matrix[0] == null || matrix[0].length == 0) return res;
+	int count = 0;
+	for (int i = 0; i < m; i++) {
+	    for (int j = 0; j < n; j++) {
+		if (i % 2 == 0) {// 奇数行
+		    res[count++] = matrix[i][j];
+		} else {// 偶数行
+		    res[count++] = matrix[i][n - 1 - j];
+		}
+	    }
+	}
+	return res;
+    }
 
 }
