8ae336741574241188ba3c21e3c00edce1aa79ff
张可新 <19329831+zkxok@users.noreply.github.com>
2019-12-31 05:21:10 +0800

    Update 455AssignCookies.java

--- src/leetcode/455AssignCookies.java
+++ src/leetcode/455AssignCookies.java
@@ -49,4 +49,20 @@ public class AssignCookies {
         }
         return res;
     }
+    
+    //*******和Leetoce392类似
+    public int findContentChildren(int[] g, int[] s) {
+        int res = 0;
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int i = 0, j = 0;
+        while (i < g.length && j < s.length) {
+            if (g[i] <= s[j]) {
+                res++;
+                i++;
+            } 
+            j++;
+        }
+        return res;
+    }
 }
