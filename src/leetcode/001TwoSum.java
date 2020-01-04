9ebf1590bb73b54836fe456515e3f82ae1f5247d
张可新 <19329831+zkxok@users.noreply.github.com>
2020-01-04 20:15:58 +0800

    Update 001TwoSum.java

--- src/leetcode/001TwoSum.java
+++ src/leetcode/001TwoSum.java
@@ -44,4 +44,25 @@ public class TwoSum {
        }
        return res;
     }
+
+    //上面解法的注释版:最优解法
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+        //int res[] = new int[2];
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(target - nums[i])) {
+                //为啥不用判断map.get(target - nums[i])和i是否相等?
+                //因为他们不可能会相等,如果map中已经存在target - nums[i]这个元素
+                //那么因为是顺序遍历，那么它的索引肯定是小于i的
+                //和两边hash的不一样
+                //你当前遍历到的这个i索引的元素(索引和元素都相等的元素))肯定是没在map里的
+                //res[0] = i;
+                //res[1] = map.get(target - nums[i]);
+                // return res;
+                return new int[] { i, map.get(target - nums[i]) };
+            }
+            map.put(nums[i], i);
+        }
+        return new int[] { -1, -1 };
+    }
 }
