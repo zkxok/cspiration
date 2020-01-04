5d9bd6fb2500ad6fbeac828b2661916d746a6c84
张可新 <19329831+zkxok@users.noreply.github.com>
2020-01-05 02:39:13 +0800

    Update 018FourSum.java

--- src/leetcode/018FourSum.java
+++ src/leetcode/018FourSum.java
@@ -21,7 +21,7 @@ public class FourSum {
      * @param target
      * @return
      */
-
+    //求和等于target的四元组的对数，不许有重复
     public List<List<Integer>> fourSum(int[] nums, int target) {
         List<List<Integer>> res = new ArrayList<>();
         if (nums.length < 4) return res;
@@ -29,11 +29,11 @@ public class FourSum {
         for (int i = 0; i < nums.length - 3; i++) {
             if (i > 0 && nums[i] == nums[i - 1]) continue;
             for (int j = i + 1; j < nums.length - 2; j++) {
-                if (j > i + 1 && nums[j] == nums[j - 1]) continue;
+                if (j > i + 1 && nums[j] == nums[j - 1]) continue;//注意这里是>j > i + 1
                 int low = j + 1, high = nums.length - 1;
                 while (low < high) {
                     int sum = nums[i] + nums[j] + nums[low] + nums[high];
-                    if (sum == target) {
+                    if (sum == target) {// 降到二维
                         res.add(Arrays.asList(nums[i], nums[j], nums[low], nums[high]));
                         while (low < high && nums[low] == nums[low + 1]) low++;
                         while (low < high && nums[high] == nums[high - 1]) high--;
