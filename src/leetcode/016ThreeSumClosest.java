67e9be15e84297c14ab1ece858e42c9c4233e2e4
张可新 <19329831+zkxok@users.noreply.github.com>
2020-01-05 00:29:26 +0800

    Update 016ThreeSumClosest.java

--- src/leetcode/016ThreeSumClosest.java
+++ src/leetcode/016ThreeSumClosest.java
@@ -24,11 +24,12 @@ public class ThreeSumClosest {
      * @return
      */
     public int threeSumClosest(int[] nums, int target) {
+        //不能为Integer.MAX_VALUE,因为target可能为负,先随便选3个数作为三数之和
         int res = nums[0] + nums[1] + nums[nums.length - 1];
         Arrays.sort(nums);
         for (int i = 0; i < nums.length - 2; i++) {
             int start = i + 1, end = nums.length - 1;
-            while (start < end) {
+            while (start < end) {//降到二维，至少还有两个数
                 int sum = nums[i] + nums[start] + nums[end];
                 if (sum > target) {
                     end--;
