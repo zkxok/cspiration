3726c6b8c34bd87bdc770dcdb881dad2ed80aa45
张可新 <19329831+zkxok@users.noreply.github.com>
2020-01-04 23:02:21 +0800

    Update 015ThreeSum.java

--- src/leetcode/015ThreeSum.java
+++ src/leetcode/015ThreeSum.java
@@ -39,7 +39,7 @@ public class ThreeSum {
         for (int i = 0; i < nums.length - 2; i++) {
             if (i > 0 && nums[i] == nums[i - 1]) continue;
             int low = i + 1, high = nums.length - 1, sum = 0 - nums[i];
-            while (low < high) {
+            while (low < high) {//降维成二维
                 if (nums[low] + nums[high] == sum) {
                     res.add(Arrays.asList(nums[i], nums[low], nums[high]));
                     while (low < high && nums[low] == nums[low + 1]) low++;//去重1
