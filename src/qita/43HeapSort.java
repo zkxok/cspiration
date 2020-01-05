885278023e26c75df2239aadf6ab178d20d01eec
张可新 <19329831+zkxok@users.noreply.github.com>
2020-01-05 21:13:09 +0800

    Update 43HeapSort.java

--- src/qita/43HeapSort.java
+++ src/qita/43HeapSort.java
@@ -1,4 +1,5 @@
 import java.util.*;
+
 public class Main {
      //测试用
      //测试OJ:https://www.nowcoder.com/questionTerminal/3385982ae71d4a1ca8bf3d03614c0325
@@ -24,7 +25,10 @@ public class Main {
         initHeap(arr);// //构建大顶堆, 从第一个非叶子结点从下至上，从右至左调整结构
         for (int i = arr.length - 1; i > 0; i--) {
             // 调整堆结构+交换堆顶元素与末尾元素
+            // 逐步将每个最大值的根节点与末尾元素交换，并且再调整二叉树，使其成为大顶堆
+            //将堆顶记录和当前未经排序子序列的最后一个记录交换
             swap(arr, 0, i);// 将堆顶元素与末尾元素进行交换,交换后最后一个元素为最大
+            // 交换之后，需要重新检查堆是否符合大顶堆，不符合则要调整
             adjustHeap(arr, 0, i);// 调整根节点满足最大堆,
         }
     }
@@ -45,13 +49,14 @@ public class Main {
             if (child + 1 < size && arr[child + 1] > arr[child]) {// 且有右子节点,如果左子结点小于右子结点，child指向右子结点
                 child++;
             }
+            // 如果父节点小于孩子结点，则需要交换
             // 如果子节点大于父节点，将子节点值赋给父节点（不用进行交换）
             if (arr[child] > temp) {// 这里的arr[child] 左右子节点中较大的那个子节点
                 arr[n] = arr[child];
                 n = child;
                 child = 2 * n + 1;
             } else {
-                break;
+                break;// 大顶堆结构未被破坏，不需要调整
             }
         }
         arr[n] = temp;// 将temp值放到最终的位置
