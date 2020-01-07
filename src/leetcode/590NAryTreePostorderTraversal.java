3e547a7f228b08f94d77b47cd1fa3f732bafe027
张可新 <19329831+zkxok@users.noreply.github.com>
2020-01-07 15:34:09 +0800

    Create 590NAryTreePostorderTraversal.java

---
+++ src/leetcode/590NAryTreePostorderTraversal.java
@@ -0,0 +1,34 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+public class Solution {
+    public List<Integer> postorder(Node root) {
+        List<Integer> res = new ArrayList<>();
+        if (root == null) return res;
+        Stack<Node> stack = new Stack<>();
+        stack.push(root);
+        while (!stack.isEmpty()) {
+            Node cur = stack.pop();
+            for (Node node : cur.children) {
+                stack.push(node);
+            }
+            res.add(0, cur.val);
+        }
+        return res;
+    }
+}
