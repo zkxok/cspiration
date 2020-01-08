79b52edb6f55bb5074df7c7c7583717e111c6825
张可新 <19329831+zkxok@users.noreply.github.com>
2020-01-09 03:23:12 +0800

    Update 230KthSmallestElementinaBST.java

--- src/leetcode/230KthSmallestElementinaBST.java
+++ src/leetcode/230KthSmallestElementinaBST.java
@@ -55,5 +55,21 @@ public class KthSmallestElementinaBST {
 		}
 		helper(root.right);
 	    }
+    //非递归式写法
+    public int kthSmallest(TreeNode root, int k) {
+        List<Integer> res = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+        TreeNode cur = root;
+        while (cur != null || !stack.isEmpty()) {
+            while (cur != null) {
+                stack.push(cur);
+                cur = cur.left;
+            }
+            cur = stack.pop();
+            res.add(cur.val);
+            cur = cur.right;
+        }
+        return res.get(k - 1);
+    }
 }
   
