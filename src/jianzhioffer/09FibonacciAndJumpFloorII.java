6b48e201a3a9c196475a188f3786ef6ed8325dc9
张可新 <19329831+zkxok@users.noreply.github.com>
2020-01-10 02:45:38 +0800

    Update and rename 09FibonacciAndJumpFloorII to 09FibonacciAndJumpFloorII.java

--- src/jianzhioffer/09FibonacciAndJumpFloorII.java
+++ src/jianzhioffer/09FibonacciAndJumpFloorII.java
@@ -1,9 +1,24 @@
-/*大家都知道斐波那契数列，现在要求输入一个整数n，请你输出斐波那契数列的第n项（从0开始，第0项为0）。
-n<=39*/
+
 
 
 
 class Solution {
+     /*大家都知道斐波那契数列，现在要求输入一个整数n，请你输出斐波那契数列的第n项（从0开始，第0项为0）。
+    n<=39*/
+    //第一题:Fibonacci数列
+    //判题OJ:https://www.nowcoder.com/practice/c6c7742f5ba7442aada113136ddea0c3?tpId=13&tqId=11160&rp=1&ru=/ta/coding-interviews&qru=/ta/coding-interviews/question-ranking
+    public int Fibonacci(int n) {
+        //first:前面2个，second:前面1个
+        int first = 1, second = 1;
+        int third = 0;
+        if(n == 1 || n ==2) return 1;
+        for(int i = 3; i <= n; i++){
+            third = first + second;
+            first = second;
+            second = third;
+        }
+        return third;
+    }
 
 //第2题:跳台阶
 //判题OJ:https://www.nowcoder.com/practice/22243d016f6b47f2a6928b4313c85387?tpId=13&tqId=11162&tPage=1&rp=1&ru=/ta/coding-interviews&qru=/ta/coding-interviews/question-ranking
