7277d734b11b228236e793da3ea204775746a368
张可新 <19329831+zkxok@users.noreply.github.com>
2020-01-09 23:09:15 +0800

    Create 059找出数组中比左边大比右边的小的元素.java

---
+++ src/qita/059找出数组中比左边大比右边的小的元素.java
@@ -0,0 +1,28 @@
+//2018腾讯内部调岗面试试题3——找出数组中比左边大比右边的小的元素
+//题解: https://cloud.tencent.com/developer/article/1176017
+//https://cloud.tencent.com/developer/article/1176017
+public class Main59 {
+    public void findElements(int[] data) {
+        int len = data.length;
+        // 从右往左，寻找每个位置及其之后的最小数
+        int rightMin[] = new int[len];
+        int r_min = data[len - 1];
+        for (int i = len - 1; i >= 0; --i) {
+            if (data[i] < r_min) {
+                r_min = data[i];
+            }
+            rightMin[i] = r_min;
+        }
+
+        // 从左往右，寻找比左边大且比右边小的数
+        int l_max = data[0];
+        for (int i = 0; i < len - 1; ++i) {
+            if (data[i] > l_max) {//比左边大
+                l_max = data[i];
+                if (data[i] < rightMin[i + 1]) {//比右边小
+                    System.out.println(data[i]);
+                }
+            }
+        }
+    }
+}
