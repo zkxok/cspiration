de6ee691a5dadb51cd4c0e25e92131403bc12ae6
张可新 <19329831+zkxok@users.noreply.github.com>
2020-01-10 01:52:54 +0800

    Update 070ClimbingStairs.java

--- src/leetcode/070ClimbingStairs.java
+++ src/leetcode/070ClimbingStairs.java
@@ -8,6 +8,7 @@ package leetcode;
  * Date : Nov, 2017
  * Description : 70. Climbing Stairs
  */
+//同剑指offer:https://www.nowcoder.com/practice/8c82a5b80378478f9484d87d1c5f12a4?tpId=13&tqId=11161&tPage=1&rp=1&ru=/ta/coding-interviews&qru=/ta/coding-interviews/question-ranking
 public class ClimbingStairs {
     /**
      * You are climbing a stair case. It takes n steps to reach to the top.
