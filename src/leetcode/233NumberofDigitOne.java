7db14382c808a3c2fe2520c630d5f0baac6a05de
张可新 <19329831+zkxok@users.noreply.github.com>
2020-01-18 05:44:58 +0800

    Update 233NumberofDigitOne.java

--- src/leetcode/233NumberofDigitOne.java
+++ src/leetcode/233NumberofDigitOne.java
@@ -38,10 +38,10 @@ public class NumberofDigitOne {
     public int countDigitOne(int n) {
         int res = 0;
         for (long m = 1; m <= n; m *= 10) {
-            long a = n / m;
+            long a = n / m;//百位的数字
             long b = n % m;
-            res += (a + 8) / 10 * m;//(a + 8) / 10 判断是m位(这里是百位)否是1
-            if (a % 10 == 1) res += b + 1;
+            res += (a + 8) / 10 * m;//(a + 8) / 10 判断是m位(这里是百位)否是1,如果百位数字a为>=2,(3141+1) *100 次.
+            if (a % 10 == 1) res += b + 1;//如果百位是1,(92+1) 次.
         }
         return res;
     }
