44a1515f5f2de94b005541e50bd8a21224de28eb
张可新 <19329831+zkxok@users.noreply.github.com>
2020-02-18 19:42:38 +0800

    Update 50二叉树的最近公共祖先.java

--- src/jianzhioffer/50二叉树的最近公共祖先.java
+++ src/jianzhioffer/50二叉树的最近公共祖先.java
@@ -1,10 +1,8 @@
  //难理解，背下来
     public static TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
         if (root == null || root == p || root == q) return root;
-
         TreeNode left = lowestCommonAncestor(root.left, p, q);
-        TreeNode right = lowestCommonAncestor(root.right, p, q);
-
+        TreeNode right = lowestCommonAncestor(root.right, p, q);    
         if (left != null && right != null) {
             return root;
         }
