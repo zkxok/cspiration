1be3efd98a111377ed3be350c2151456938ac7ac
张可新 <19329831+zkxok@users.noreply.github.com>
2020-02-18 17:49:02 +0800

    Update 25二叉树中和为某一值的路径.java

--- src/jianzhioffer/25二叉树中和为某一值的路径.java
+++ src/jianzhioffer/25二叉树中和为某一值的路径.java
@@ -15,4 +15,24 @@ public class Solution {
         list.remove(list.size()-1);
         return listAll;
     }
+    
+    ******回溯标准写法********
+    public ArrayList<ArrayList<Integer>> FindPath(TreeNode root, int target) {
+		ArrayList<ArrayList<Integer>> res = new ArrayList<>();
+		helper(res, new ArrayList<>(), root, target);
+		return res;
+	}
+
+	public void helper(ArrayList<ArrayList<Integer>> res, ArrayList<Integer> tempList, TreeNode root, int target) {
+		if (root == null) return;
+		tempList.add(root.val);
+		target -= root.val;// 一直减，减到0证明刚好等于target,且没有子节点证明是一整条路径(已经到达叶结点)
+		if (target == 0 && root.left == null && root.right == null) {
+			res.add(new ArrayList<>(tempList));
+		}
+		helper(res, tempList, root.left, target);
+		helper(res, tempList, root.right, target);
+		tempList.remove(tempList.size() - 1);
+
+	}
 }
