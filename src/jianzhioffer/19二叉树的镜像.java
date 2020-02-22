44d189d2211403e8891d2cd894b7ecd72a9641d6
张可新 <19329831+zkxok@users.noreply.github.com>
2020-02-22 23:12:28 +0800

    Update 19二叉树的镜像.java

--- src/jianzhioffer/19二叉树的镜像.java
+++ src/jianzhioffer/19二叉树的镜像.java
@@ -1,15 +1,6 @@
 public class Solution {
     public TreeNode mirrorTree(TreeNode root) {
         if (root == null) return root;
-        TreeNode left = mirrorTree(root.left);
-        TreeNode right = mirrorTree(root.right);
-        root.left = right;
-        root.right = left;
-        return root;
-    }
-/*****************************/
-    public TreeNode mirrorTree(TreeNode root) {
-        if (root == null) return root;
         Queue<TreeNode> queue = new LinkedList<>();
         queue.offer(root);
         while (!queue.isEmpty()) {
@@ -20,6 +11,15 @@ public class Solution {
             if (cur.left != null) queue.offer(cur.left);
             if (cur.right != null) queue.offer(cur.right);
         }
+        return root;
+    }
+/*****************************/
+    public TreeNode mirrorTree(TreeNode root) {
+        if (root == null) return root;
+        TreeNode left = mirrorTree(root.left);
+        TreeNode right = mirrorTree(root.right);
+        root.left = right;
+        root.right = left;
         return root;
     }
 } 
