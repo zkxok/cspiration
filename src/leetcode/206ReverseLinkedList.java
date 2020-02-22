fe7cb1ff21106c23e89453c8d87e102e04d7a145
张可新 <19329831+zkxok@users.noreply.github.com>
2020-02-22 17:41:53 +0800

    Update 206ReverseLinkedList.java

--- src/leetcode/206ReverseLinkedList.java
+++ src/leetcode/206ReverseLinkedList.java
@@ -15,7 +15,7 @@ public class ReverseLinkedList {
      * @return
      */
     public static ListNode reverseList(ListNode head) {
-        //if(head==null) return head;这样写也行
+        //if(head==null) return head;这样写也行，下面一行注释掉也行
         if (head == null || head.next == null) return head;
         ListNode pre = null;
         while (head != null) {
