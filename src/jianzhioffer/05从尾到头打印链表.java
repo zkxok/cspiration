92c2b80254e539706abbac6bb00ba7669d4353e2
张可新 <19329831+zkxok@users.noreply.github.com>
2020-02-22 18:35:58 +0800

    Create 05从尾到头打印链表.java

---
+++ src/jianzhioffer/05从尾到头打印链表.java
@@ -0,0 +1,14 @@
+public class Solution {
+   public ArrayList<Integer> printListFromTailToHead(ListNode listNode) {
+		ArrayList<Integer> res = new ArrayList<Integer>();
+		Stack<Integer> stack = new Stack<Integer>();
+		while(listNode != null) {
+			stack.push(listNode.val);
+            listNode = listNode.next;
+		}
+		for (int i = 0; stack.size()!=0; i++) {
+			res.add(stack.pop());
+		}
+		return res;
+	}
+}
