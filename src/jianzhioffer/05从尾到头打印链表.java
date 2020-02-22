57d9d489c9c97a0002071055c77a561fffc7e1d7
张可新 <19329831+zkxok@users.noreply.github.com>
2020-02-22 18:51:13 +0800

    Update 05从尾到头打印链表.java

--- src/jianzhioffer/05从尾到头打印链表.java
+++ src/jianzhioffer/05从尾到头打印链表.java
@@ -1,14 +1,23 @@
 public class Solution {
-   public ArrayList<Integer> printListFromTailToHead(ListNode listNode) {
-		ArrayList<Integer> res = new ArrayList<Integer>();
-		Stack<Integer> stack = new Stack<Integer>();
-		while(listNode != null) {
-			stack.push(listNode.val);
+    public ArrayList<Integer> printListFromTailToHead(ListNode listNode) {
+        ArrayList<Integer> res = new ArrayList<Integer>();
+        Stack<Integer> stack = new Stack<Integer>();
+        while (listNode != null) {
+            stack.push(listNode.val);
             listNode = listNode.next;
-		}
-		for (int i = 0; stack.size()!=0; i++) {
-			res.add(stack.pop());
-		}
-		return res;
-	}
+        }
+        for (int i = 0; stack.size() != 0; i++) {
+            res.add(stack.pop());
+        }
+        return res;
+    }
+
+    class ListNode {
+        int val;
+        ListNode next = null;
+
+        ListNode(int val) {
+            this.val = val;
+        }
+    }
 }
