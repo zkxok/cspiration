257b77af8b85e54e0cd8f23863a0583de194b380
张可新 <19329831+zkxok@users.noreply.github.com>
2020-02-22 19:52:23 +0800

    Create 12打印1到最大的N位数.java

---
+++ src/jianzhioffer/12打印1到最大的N位数.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int[] printNumbers(int n) {
+        int max = (int)Math.pow(10, n);
+        int[] ans = new int[max - 1];
+        for(int i = 1 ; i <= max - 1 ; i++){
+            ans[i - 1] = i;
+        }
+        return ans;
+    }
+}
