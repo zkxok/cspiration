739dbc8b3c2de3c57e2ec65d9e8e753304bb7ea0
张可新 <19329831+zkxok@users.noreply.github.com>
2020-02-22 18:29:48 +0800

    Update 03二维数组中的查找.java

--- src/jianzhioffer/03二维数组中的查找.java
+++ src/jianzhioffer/03二维数组中的查找.java
@@ -3,7 +3,6 @@ public class Searcha2DMatrixII {//同LC240 搜索二维矩阵 II
         if (matrix == null || matrix.length == 0) return false;
         int row = 0;
         int col = matrix[0].length - 1;
-
         while (col >= 0 && row <= matrix.length - 1) {
             if (target == matrix[row][col]) {
                 return true;
@@ -14,5 +13,21 @@ public class Searcha2DMatrixII {//同LC240 搜索二维矩阵 II
             }
         }
         return false;
+    }
+    
+    //*********************
+    public boolean Find(int target, int [][] array) {
+		 int i=0;
+	     int j=array[0].length-1;
+	     while(i<=array.length-1&&j>=0){//&&target!=array[i][j]
+	         if(target==array[i][j]){
+	            return true;
+	         }else if(target<array[i][j]){//小于上移
+	            j--;
+	         }else if(target>array[i][j]){
+	            i++;
+	         }
+	     }
+	     return false;
     }
 }
