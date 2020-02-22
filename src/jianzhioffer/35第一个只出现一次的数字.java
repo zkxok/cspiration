fb02c16967b961b48513fbd2fe0b00a0d7b80d47
张可新 <19329831+zkxok@users.noreply.github.com>
2020-02-23 04:22:33 +0800

    Update 35第一个只出现一次的数字.java

--- src/jianzhioffer/35第一个只出现一次的数字.java
+++ src/jianzhioffer/35第一个只出现一次的数字.java
@@ -1,12 +1,14 @@
-public int firstUniqChar(String s) {
-    int[] count = new int[26];
-    for (int i = 0; i < s.length(); i++) {
-        count[s.charAt(i) - 'a']++;
-    }
-    for (int i = 0; i < s.length(); i++) {
-        if (count[s.charAt(i) - 'a'] == 1) {
-            return i;
+class Solution {
+    public char firstUniqChar(String s) {
+        int[] count = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            count[s.charAt(i) - 'a']++;
         }
+        for (int i = 0; i < s.length(); i++) {
+            if (count[s.charAt(i) - 'a'] == 1) {
+                return s.charAt(i) ;
+            }
+        }
+        return ' ';
     }
-    return -1;
 }
