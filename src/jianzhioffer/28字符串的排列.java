7c40326b3e741ef3a0d95366c88b5868e4bf62a9
张可新 <19329831+zkxok@users.noreply.github.com>
2020-02-23 02:02:18 +0800

    Update 28字符串的排列.java

--- src/jianzhioffer/28字符串的排列.java
+++ src/jianzhioffer/28字符串的排列.java
@@ -1,33 +1,37 @@
-public String[] permutation(String s) {
-		// 直接用string[] 无法判断个数
-		ArrayList<String> list = new ArrayList<>();
-		helper(s.toCharArray(), 0, list);
-		String[] res = new String[list.size()];
-		for (int i = 0; i < list.size(); i++) {
-			res[i] = list.get(i);
-		}
-		return res;
-	}
+public class Solution {
 
-	private void helper(char[] ch, int i, ArrayList<String> list) {
-		if (i == ch.length) {
-			list.add(String.valueOf(ch));
-			return;
-		}
-		HashSet<Character> set = new HashSet<>();
-		for (int j = i; j < ch.length; j++) {
-			if (j == i || !set.contains(ch[j])) {
-				set.add(ch[j]);
-				swap(ch, i, j);
-				helper(ch, i + 1, list);
-				swap(ch, j, i);
-			}
-		}
+    public String[] permutation(String s) {
+        // 直接用string[] 无法判断个数
+        ArrayList<String> list = new ArrayList<>();
+        helper(s.toCharArray(), 0, list);
+        String[] res = new String[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            res[i] = list.get(i);
+        }
+        return res;
+    }
 
-	}
+    public void helper(char[] ch, int i, ArrayList<String> list) {
+        if (i == ch.length) {
+            list.add(String.valueOf(ch));
+            return;
+        }
+        HashSet<Character> set = new HashSet<>();
+        for (int j = i; j < ch.length; j++) {
+            if (j == i || !set.contains(ch[j])) {
+                set.add(ch[j]);
+                swap(ch, i, j);
+                helper(ch, i + 1, list);
+                swap(ch, j, i);
+            }
+        }
 
-	private void swap(char[] ch, int i, int j) {
-		char tmp = ch[i];
-		ch[i] = ch[j];
-		ch[j] = tmp;
-	}
+    }
+
+    public void swap(char[] ch, int i, int j) {
+        char tmp = ch[i];
+        ch[i] = ch[j];
+        ch[j] = tmp;
+    }
+
+}
