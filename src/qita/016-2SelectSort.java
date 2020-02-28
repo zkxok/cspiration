be4444b8b3bcdd8ffb94df6f046d79783304684a
张可新 <19329831+zkxok@users.noreply.github.com>
2020-02-29 04:43:10 +0800

    Update 016-2SelectSort.java

--- src/qita/016-2SelectSort.java
+++ src/qita/016-2SelectSort.java
@@ -1,18 +1,18 @@
-public static void selectSort(long arr[]) {
-		int minIndex;
-		long temp;
-		int n = arr.length;
-		for (int i = 0; i < n; i++) {
-			minIndex = i;
-			for (int j = i + 1; j < n; j++) {
-				if (arr[j] < arr[minIndex]) {
-					minIndex = j;
-				}
-			}
-			if (minIndex != i) {
-				temp = arr[minIndex];
-				arr[minIndex] = arr[i];
-				arr[i] = temp;
-			}
-		}
-	}
+   public static void selectSort(long arr[]) {
+        int minIndex;
+        long temp;
+        int n = arr.length;
+        for (int i = 0; i < n; i++) {
+            minIndex = i;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            if (minIndex != i) {
+                temp = arr[minIndex];
+                arr[minIndex] = arr[i];
+                arr[i] = temp;
+            }
+        }
+    }
