25fc4415e5a7c315b6bddb950695e207ef5975f0
张可新 <19329831+zkxok@users.noreply.github.com>
2020-02-29 05:03:43 +0800

    Update 016-1BubbleSort.java

--- src/qita/016-1BubbleSort.java
+++ src/qita/016-1BubbleSort.java
@@ -1,6 +1,7 @@
 十大经典排序算法:
 https://www.runoob.com/w3cnote/ten-sorting-algorithm.html
 https://github.com/francistao/LearningNotes/blob/master/Part3/Algorithm/Sort/%E9%9D%A2%E8%AF%95%E4%B8%AD%E7%9A%84%2010%20%E5%A4%A7%E6%8E%92%E5%BA%8F%E7%AE%97%E6%B3%95%E6%80%BB%E7%BB%93.md
+https://baike.baidu.com/item/%E6%8E%92%E5%BA%8F%E7%AE%97%E6%B3%95%E7%A8%B3%E5%AE%9A%E6%80%A7
 //冒泡排序:Time:O(n^2),Space:(1) ,最好Time:O(n),最坏Time:O(n^2) 稳定  
 public static void bubbleSort(long[] arr) {
     int n = arr.length;
