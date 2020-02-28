cbcce5b2c7cfe50bc5c8b00a6207b773ab1a134e
张可新 <19329831+zkxok@users.noreply.github.com>
2020-02-29 04:56:44 +0800

    Update 016-1BubbleSort.java

--- src/qita/016-1BubbleSort.java
+++ src/qita/016-1BubbleSort.java
@@ -1,17 +1,18 @@
-    public static void bubbleSort(long[] arr) {
-        int n = arr.length;
-        boolean exchange = true;
-        long temp;
-        for (int i = 0; i < n - 1; i++) {
-            exchange = false;
-            for (int j = n - 1; j > i; j--) {
-                if (arr[j - 1] > arr[j]) {
-                    temp = arr[j - 1];
-                    arr[j - 1] = arr[j];
-                    arr[j] = temp;
-                    exchange=true;
-                }
+//冒泡排序:Time:O(n^2),Space:(1) ,最好Time:O(n),最坏Time:O(n^2)不稳定  
+public static void bubbleSort(long[] arr) {
+    int n = arr.length;
+    boolean exchange = true;
+    long temp;
+    for (int i = 0; i < n - 1; i++) {
+        exchange = false;
+        for (int j = n - 1; j > i; j--) {
+            if (arr[j - 1] > arr[j]) {
+                temp = arr[j - 1];
+                arr[j - 1] = arr[j];
+                arr[j] = temp;
+                exchange=true;
             }
-            if (!exchange) return;
         }
+        if (!exchange) return;
     }
+ }
