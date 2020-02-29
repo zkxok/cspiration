79770fd55b53e35e4576441a6d6b55ab99bb52f7
张可新 <19329831+zkxok@users.noreply.github.com>
2020-03-01 00:00:18 +0800

    Update 59对称的二叉树.java

--- src/jianzhioffer/59对称的二叉树.java
+++ src/jianzhioffer/59对称的二叉树.java
@@ -1,1 +1,15 @@
+public class SymmetricTree {
+    //time : O(n)
+    //space : O(n)
+    public static boolean isSymmetric(TreeNode root) {
+        if (root == null) return true;
+        return helper(root.left, root.right);
+    }
 
+    public static boolean helper(TreeNode p, TreeNode q) {
+        if (p == null && q == null) return true;
+        if (p == null || q == null) return false;
+        if (p.val != q.val) return false;
+        return helper(p.left, q.right) && helper(p.right, q.left);
+    }
+}
