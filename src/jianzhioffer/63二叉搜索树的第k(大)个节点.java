edcdbb055c7b6a3c2e3475116aa57ff02b49f727
张可新 <19329831+zkxok@users.noreply.github.com>
2020-03-01 00:43:08 +0800

    Update and rename 63二叉搜索树的第k个节点.java to 63二叉搜索树的第k(大)个节点.java

---
+++ src/jianzhioffer/63二叉搜索树的第k(大)个节点.java
@@ -0,0 +1,16 @@
+class Solution {//LC是第K小
+    int count = 0;
+    int res = 0;
+    public int kthLargest(TreeNode root, int k) {
+        count = k;
+        helper(root);
+        return res;
+    }
+    public void helper(TreeNode root) {
+        if (root == null) return;
+        helper(root.right);
+        count--;
+        if (count == 0) res = root.val;
+        helper(root.left);
+    }
+}
