94a708d76c61020cf44e992876d205475baf80c9
张可新 <19329831+zkxok@users.noreply.github.com>
2020-03-01 00:27:36 +0800

    Update 230KthSmallestElementinaBST.java

--- src/leetcode/230KthSmallestElementinaBST.java
+++ src/leetcode/230KthSmallestElementinaBST.java
@@ -15,46 +15,45 @@ public class KthSmallestElementinaBST {
      * @param k
      * @return
      */
+    //中序遍历顺序的下一个结点
+    int count = 0;
+    int res = 0;
+    public int kthSmallest(TreeNode root, int k) {
+        count = k;
+        helper(root);
+        return res;
+    }
+    public void helper(TreeNode root) {
+        if (root == null)
+            return;
+        helper(root.left);
+        count--;
+        if (count == 0)
+            res = root.val;
+        helper(root.right);
+    }
 
-	int count = 0;
-	int res = 0;
-
-	public int kthSmallest(TreeNode root, int k) {
-		count = k;
-		helper(root);
-		return res;
-	}
-
-	public void helper(TreeNode root) {
-		if (root == null) return;
-		helper(root.left);
-		count--;
-		if (count == 0) res = root.val;
-		helper(root.right);
-	}
+    // 一个不错的解法:
+    // https://leetcode-cn.com/problems/kth-smallest-element-in-a-bst/solution/javashu-de-zhong-xu-bian-li-fang-fa-di-gui-he-fei-/
+    // 个人简化版
+    int res;
+    int count;
+    public int kthSmallest(TreeNode root, int k) {
+        count = k;
+        helper(root);
+        return res;
+    }
+    public void helper(TreeNode root) {
+        if (root == null) return;
+        helper(root.left);
+        count--;
+        if (count == 0) {
+            res = root.val;
+            return;
+        }
+        helper(root.right);
+    }
 	
-	//一个不错的解法:
-	//https://leetcode-cn.com/problems/kth-smallest-element-in-a-bst/solution/javashu-de-zhong-xu-bian-li-fang-fa-di-gui-he-fei-/
-
-	  //个人简化版
-	    int res;
-	    int count;
-	    public int kthSmallest(TreeNode root, int k) {
-		count = k;
-		helper(root);
-		return res;
-	    }
-
-	    public void helper(TreeNode root){
-		if(root==null) return;
-		helper(root.left);
-		count--;
-		if(count==0) {
-		    res = root.val;
-		    return;
-		}
-		helper(root.right);
-	    }
     //非递归式写法
     public int kthSmallest(TreeNode root, int k) {
         List<Integer> res = new ArrayList<>();
