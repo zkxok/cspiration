edc78a4cf07d560f2d4e837da385993c2a7b75df
张可新 <19329831+zkxok@users.noreply.github.com>
2020-03-01 00:49:17 +0800

    Update 63二叉搜索树的第k(大)个节点.java

--- src/jianzhioffer/63二叉搜索树的第k(大)个节点.java
+++ src/jianzhioffer/63二叉搜索树的第k(大)个节点.java
@@ -13,4 +13,21 @@ class Solution {//LC是第K小,这里是第K大
         if (count == 0) res = root.val;
         helper(root.left);
     }
+    
+     //非递归式写法
+    public int kthLargest(TreeNode root, int k) {
+        List<Integer> res = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+        TreeNode cur = root;
+        while (cur != null || !stack.isEmpty()) {
+            while (cur != null) {
+                stack.push(cur);
+                cur = cur.right;
+            }
+            cur = stack.pop();
+            res.add(cur.val);
+            cur = cur.left;
+        }
+        return res.get(k - 1);
+    }
 }
