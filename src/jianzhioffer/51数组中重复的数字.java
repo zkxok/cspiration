15b9cafb23ee7b59cedf77fb05f14dce13fde56a
张可新 <19329831+zkxok@users.noreply.github.com>
2020-02-29 21:43:36 +0800

    Update 51数组中重复的数字.java

--- src/jianzhioffer/51数组中重复的数字.java
+++ src/jianzhioffer/51数组中重复的数字.java
@@ -1,4 +1,14 @@
 class Solution {
+    
+    public int findRepeatNumber(int[] nums) {
+        int[] count = new int[nums.length];
+        for(int i = 0; i < nums.length; i++){
+            count[nums[i]]++;
+            if(count[nums[i]] > 1) return nums[i];
+        }
+        return -1;
+    }
+    /*********************************/
     public int findRepeatNumber(int[] nums) {
         Set<Integer> set = new HashSet<Integer>();
         int repeat = -1;
