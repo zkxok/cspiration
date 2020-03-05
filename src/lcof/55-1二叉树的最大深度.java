0d42a008e9df0dbdc367442fd9121065dcae50f2
张可新 <19329831+zkxok@users.noreply.github.com>
2020-03-06 03:11:00 +0800

    Update 55-1二叉树的最大深度.java

--- src/lcof/55-1二叉树的最大深度.java
+++ src/lcof/55-1二叉树的最大深度.java
@@ -1,39 +1,33 @@
 public class BinaryTreeLevelOrderTraversal {
      //time : O(n);
      //space : O(n);
-    public static List<List<Integer>> levelOrder(TreeNode root) {
-        List<List<Integer>> res = new ArrayList<>();
-        if (root == null) return res;
+    public int maxDepth(TreeNode root) {
+        if (root == null) return 0;
         Queue<TreeNode> queue = new LinkedList<>();
         queue.offer(root);
+        int level=0;
         while (!queue.isEmpty()) {
             int size = queue.size();
-            List<Integer> list = new ArrayList<>();
             for (int i = 0; i < size; i++) {
                 TreeNode cur = queue.poll();
                 if (cur.left != null) queue.offer(cur.left);
                 if (cur.right != null) queue.offer(cur.right);
-                list.add(cur.val);
             }
-            res.add(list);
+            level++;
         }
-        return res;
+        return level;
     }
-  
-    //********递归版*********
-    public static List<List<Integer>> levelOrder2(TreeNode root) {
-        List<List<Integer>> res = new ArrayList<>();
-        if (root == null) return res;
-        helper(res, root, 0);
-        return res;
+     
+    //递归版1
+    public static int maxDepth(TreeNode root) {
+        if (root == null) return 0;
+        return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
     }
-    public static void helper(List<List<Integer>> res, TreeNode root, int level) {
-        if (root == null) return;
-        if (level >= res.size()) {
-            res.add(new ArrayList<>());
-        }
-        res.get(level).add(root.val);
-        helper(res, root.left, level + 1);
-        helper(res, root.right, level + 1);
+    //递归版2
+    public static int maxDepth2(TreeNode root) {
+        if (root == null) return 0;
+        int l = maxDepth2(root.left) + 1;
+        int r = maxDepth2(root.right) + 1;
+        return Math.max(l, r);
     }
 }
