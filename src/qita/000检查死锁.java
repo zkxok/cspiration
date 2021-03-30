8872e983f008e78f9164b6703950e5f4e4dc6ef9
ruok <19329831+zkxok@users.noreply.github.com>
2021-03-30 13:25:12 +0800

    Update 000检查死锁.java

--- src/qita/000检查死锁.java
+++ src/qita/000检查死锁.java
@@ -3,163 +3,101 @@ package test;
 import java.util.*;
 
 //https://www.geeksforgeeks.org/detect-cycle-in-a-graph/
-public class TestMain {
+public class Main {
 
-	public static void main(String[] args) {
-		MyThread t1 = new MyThread();
-		t1.holdingid = new int[]{1,2,3};
-		t1.waitingid = 4;
-		MyThread t2 = new MyThread();
-		t2.holdingid = new int[]{4,5,6};
-		t2.waitingid = 7;
-		MyThread t3 = new MyThread();
-		t3.holdingid = new int[]{7,8,9};
-		t3.waitingid = 11;
-		MyThread t4 = new MyThread();
-		t4.holdingid = new int[]{10,11,12};
-		t4.waitingid = 15;
-		MyThread t5 = new MyThread();
-		t5.holdingid = new int[]{13,14,15};
-		t5.waitingid = 3;
-		MyThread thread[] = { t1, t2, t3, t4, t5 };
-		System.out.println(isDeadLock(thread));
-	}
 
-	public static class MyThread {
-		int waitingid;
-		int holdingid[];
-	}
+    public static void main(String[] args) {
+        MyThread threads[] = new MyThread[3];
+        MyThread t0 = new MyThread();
+        t0.holdingid = new int[]{0, 1, 2, 3};
+        t0.waitingid = 4;
 
-	public static class ThreadId {
-		int intId;
-		String stringId;
-		boolean isIntId;
+        MyThread t1 = new MyThread();
+        t1.holdingid = new int[]{4, 5, 6};
+        t1.waitingid = 7;
 
-		public ThreadId(String stringId) {
-			this.stringId = stringId;
-			this.isIntId = false;
-		}
+        MyThread t2 = new MyThread();
+        t2.holdingid = new int[]{7, 8};
+        t2.waitingid = 4;
+        threads[0] = t0;
+        threads[1] = t1;
+        threads[2] = t2;
+        boolean isDeadLock = isDeadLock(threads);
+        System.out.println("isDeadLock:" + isDeadLock);
+    }
 
-		public ThreadId(int intId) {
-			this.intId = intId;
-			this.isIntId = true;
-		}
+    //检查是否死锁
+    static boolean isDeadLock(MyThread threads[]) {
+        HashSet<Integer> set = new HashSet<>();
+        LinkedList<int[]> linkedList = new LinkedList();
+        for (int i = 0; i < threads.length; i++) {
+            int hidArr[] = threads[i].holdingid;
+            int wid = threads[i].waitingid;
+            set.add(wid);
+            for (int j = 0; j < hidArr.length; j++) {
+                set.add(hidArr[j]);
+                int arr[] = new int[2];
+                arr[0] = hidArr[j];
+                arr[1] = wid;
+                linkedList.add(arr);
+            }
+        }
+        int[] matrix[] = new int[linkedList.size()][2];
+        int count = 0;
+        System.out.println("matrix:" + matrix.length);
+        for (int arr[] : linkedList) {
+            matrix[count] = arr;
+            count++;
+        }
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                System.out.println(matrix[i][j]);
+            }
+            System.out.println("---------");
 
-		@Override
-		public boolean equals(Object o) {
-			if (this == o)
-				return true;
-			if (!(o instanceof ThreadId))
-				return false;
-			ThreadId threadId = (ThreadId) o;
-			return intId == threadId.intId && isIntId == threadId.isIntId && stringId.equals(threadId.stringId);
-		}
+        }
+        System.out.println("set.size:" + set.size());
+        return !canFinish(set.size(), matrix);
+    }
 
-		@Override
-		public int hashCode() {
-			return Objects.hash(intId, stringId, isIntId);
-		}
-	}
+    static class MyThread {
+        int waitingid;
+        int holdingid[];
+    }
 
-	// 检查是否死锁
-	static boolean isDeadLock(MyThread threads[]) {
-		Map<ThreadId, Set<ThreadId>> map = createMap(threads);
-		Map<ThreadId, Integer> threadIdIndexMap = getAllThreadIdIndexMap(map);
-		Map<Integer, ThreadId> indexThreadIdMap = getAllIndexThreadIdMap(map);
-		int numCourses = map.size();
-		int[][] prerequisites = new int[numCourses][];
 
-		for (int i = 0; i < numCourses; i++) {
-			int[] lines = null;
-			ThreadId threadId = indexThreadIdMap.get(i);
-			Set<ThreadId> fromSet = map.get(threadId);
-			lines = new int[fromSet.size()];
+    public static boolean canFinish(int numCourses, int[][] prerequisites) {
+        int[] indegree = new int[numCourses];
+        int res = numCourses;
+        //计算所有节点pair[0]的入度
+        for (int[] pair : prerequisites) {
+            indegree[pair[0]]++;
+        }
 
-			int index = 0;
-			for (ThreadId from : fromSet) {
-				lines[index] = threadIdIndexMap.get(from);
-				index++;
-			}
-			prerequisites[i] = lines;
-		}
-		return canFinish(numCourses, prerequisites);
-	}
+        Queue<Integer> queue = new LinkedList<>();
+        for (int i = 0; i < indegree.length; i++) {
+            //将入度为0的加入队列
+            if (indegree[i] == 0) {
+                queue.offer(i);
+            }
+        }
 
-	public static Map<ThreadId, Integer> getAllThreadIdIndexMap(Map<ThreadId, Set<ThreadId>> map) {
-		Map<ThreadId, Integer> allThreadIdIndexMap = new HashMap<>();
-		int index = 0;
-		for (ThreadId threadId : map.keySet()) {
-			allThreadIdIndexMap.put(threadId, index);
-			index++;
-		}
-		return allThreadIdIndexMap;
-	}
+        while (!queue.isEmpty()) {
+            //将每一个元素(入度为0的)出队列,当图中所有元素都入队列后且都出队列，那么没有环
+            int pre = queue.poll();
+            res--;//记录出队列的次数，当所有元素都入队列且出队列，那么res==0,没有环，否则有环
+            for (int[] pair : prerequisites) {
+                if (pair[1] == pre) {//找到入度为0节点的邻接节点
+                    indegree[pair[0]]--;//将其邻接节点入度-1
+                    if (indegree[pair[0]] == 0) {//入度-1后又有可能入度变成0，将入度为0的加入队列
+                        queue.offer(pair[0]);
+                    }
+                }
+            }
+        }
+        //res==0，说明所有的元素都入过队列，且出了队列
+        //res==0,说明有向图无环,可以完成选修课程
+        return res == 0;
+    }
 
-	public static Map<Integer, ThreadId> getAllIndexThreadIdMap(Map<ThreadId, Set<ThreadId>> map) {
-		Map<Integer, ThreadId> allThreadIdIndexMap = new HashMap<>();
-		int index = 0;
-		for (ThreadId threadId : map.keySet()) {
-			allThreadIdIndexMap.put(index, threadId);
-			index++;
-		}
-		return allThreadIdIndexMap;
-	}
-
-	public static Map<ThreadId, Set<ThreadId>> createMap(MyThread[] threads) {
-		Map<ThreadId, Set<ThreadId>> map = new HashMap<>();
-		for (int i = 0; i < threads.length; i++) {
-			ThreadId from = new ThreadId(String.valueOf(i));
-			ThreadId to = new ThreadId(threads[i].waitingid);
-			Set<ThreadId> lineList = map.get(to);
-			if (lineList == null) {
-				lineList = new HashSet<>();
-			}
-			lineList.add(from);
-
-			map.put(to, lineList);
-			for (int j = 0; j < threads[i].holdingid.length; j++) {
-				ThreadId fromInner = new ThreadId(threads[i].holdingid[j]);
-				ThreadId toInner = fromInner;
-				Set<ThreadId> lineListInner = map.get(toInner);
-				if (lineListInner == null) {
-					lineListInner = new HashSet<>();
-				}
-				lineListInner.add(fromInner);
-				map.put(toInner, lineListInner);
-			}
-		}
-		return map;
-	}
-
-	public static boolean canFinish(int numCourses, int[][] prerequisites) {
-		List<List<Integer>> graph = new ArrayList<>();
-		int[] in = new int[numCourses];
-		for (int i = 0; i < numCourses; i++) {
-			graph.add(new ArrayList<>());
-		}
-		for (int[] edge : prerequisites) {
-			graph.get(edge[1]).add(edge[0]);
-			in[edge[0]]++;
-		}
-
-		Queue<Integer> queue = new LinkedList<>();
-		for (int i = 0; i < numCourses; i++) {
-			if (in[i] == 0)
-				queue.add(i);
-		}
-
-		while (!queue.isEmpty()) {
-			int i = queue.poll();
-			for (int a : graph.get(i)) {
-				in[a]--;
-				if (in[a] == 0)
-					queue.add(a);
-			}
-		}
-
-		for (int i = 0; i < numCourses; i++)
-			if (in[i] != 0)
-				return false;
-		return true;
-	}
 }
